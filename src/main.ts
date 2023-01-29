8d0ba5fb9feb74d5f4813aedd7c4b4f6e8b17181
joaopedroaquino <aqerhack@gmail.com>
2023-01-29 17:48:32 -0300

    refactor function isSunday

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 // @ts-nocheck
-// calculate ride
+function isSunday (date){
+	return date.getDay() === 0
+}
 export function calculateRide(segments) {
 	let fare = 0;
 	for (const segment of segments) {
 		if (segment.distance != null && segment.distance != undefined && typeof segment.distance === "number" && segment.distance > 0) {
 			if (segment.date != null && segment.date != undefined && segment.date instanceof Date && segment.date.toString() !== "Invalid Date") {
-				// overnight
-				if (segment.date.getHours() >= 22 || segment.date.getHours() <= 6) {
+				const overnight = segment.date.getHours() >= 22 || segment.date.getHours() <= 6
+				if (overnight) {
 					// not sunday
 					if (segment.date.getDay() !== 0) {
 						fare += segment.distance * 3.90;
@@ -16,7 +18,7 @@ export function calculateRide(segments) {
 					}
 				} else {
 					// sunday
-					if (segment.date.getDay() === 0) {
+					if (isSunday(segment.date)) {
 						fare += segment.distance * 2.9;
 					} else {
 						fare += segment.distance * 2.10;
