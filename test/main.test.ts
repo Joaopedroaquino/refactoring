6ee8c96c37ee187509991376e02725f2b8fab359
joaopedroaquino <aqerhack@gmail.com>
2023-01-28 22:32:06 -0300

    create first test

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,12 +1,17 @@
 import { calc } from "../src/main";
 
 test("Deve fazer uma corrida em um dia de semana e em horario normal", function () {
+    //given, arrange
     const segments = [
-        {dist: 10,
-        ds: new Date("2021-03-10t10:00:00")}
-    ]
-   const fare = calc(segments);
-   expect(fare).toBe(21)
+        {
+            dist: 10,
+            ds: new Date("2021-03-10t10:00:00")
+        }
+    ];
+    //when, act
+    const fare = calc(segments);
+    //then, assert
+    expect(fare).toBe(21);
 
 });
 
