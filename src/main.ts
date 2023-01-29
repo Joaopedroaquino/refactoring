2289f6b8cfefc5746ccb2e503a667df7a2c7332f
joaopedroaquino <aqerhack@gmail.com>
2023-01-29 18:04:46 -0300

    refactor isOvernight func

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@
 function isSunday (date){
 	return date.getDay() === 0
 }
+function isOvernight(date){
+	return date.getHours() >= 22 || date.getHours() <= 6
+
+}
 export function calculateRide(segments) {
 	let fare = 0;
 	for (const segment of segments) {
 		if (segment.distance != null && segment.distance != undefined && typeof segment.distance === "number" && segment.distance > 0) {
 			if (segment.date != null && segment.date != undefined && segment.date instanceof Date && segment.date.toString() !== "Invalid Date") {
-				const overnight = segment.date.getHours() >= 22 || segment.date.getHours() <= 6
-				if (overnight) {
+			
+				if (isOvernight(segment.date)) {
 					if (isSunday(segment.date)) {
 						fare += segment.distance * 5;
 
