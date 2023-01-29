97cc5261a5570db455b7e93ba97f7bf6fd265ac8
joaopedroaquino <aqerhack@gmail.com>
2023-01-28 22:41:15 -0300

    pass test if is night

--- test/main.test.ts
+++ test/main.test.ts
@@ -5,13 +5,28 @@ test("Deve fazer uma corrida em um dia de semana e em horario normal", function 
     const segments = [
         {
             dist: 10,
-            ds: new Date("2021-03-10t10:00:00")
+            ds: new Date("2021-03-10T10:00:00")
         }
     ];
     //when, act
     const fare = calc(segments);
     //then, assert
     expect(fare).toBe(21);
+
+});
+
+test("Deve fazer uma corrida em um dia de semana a noite", function () {
+    //given, arrange
+    const segments = [
+        {
+            dist: 10,
+            ds: new Date("2021-03-10T23:00:00")
+        }
+    ];
+    //when, act
+    const fare = calc(segments);
+    //then, assert
+    expect(fare).toBe(39);
 
 });
 
