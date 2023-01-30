de3bc807f4dfc3138c8e8868d08777be9d2d0b93
joaopedroaquino <aqerhack@gmail.com>
2023-01-30 20:25:31 -0300

    remove dead code

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,89 +1,71 @@
 import { calculateRide } from "../src/main";
 
 test("Deve fazer uma corrida em um dia de semana e em horario normal", function () {
-    //given, arrange
     const segments = [
         {
             distance: 10,
             date: new Date("2021-03-10T10:00:00")
         }
     ];
-    //when, act
     const fare = calculateRide(segments);
-    //then, assert
     expect(fare).toBe(21);
 
 });
 
 test("Deve fazer uma corrida em um dia de semana a noite", function () {
-    //given, arrange
     const segments = [
         {
             distance: 10,
             date: new Date("2021-03-10T23:00:00")
         }
     ];
-    //when, act
     const fare = calculateRide(segments);
-    //then, assert
     expect(fare).toBe(39);
 
 });
 test("Deve fazer uma corrida em um domingo e em um horario normal", function () {
-    //given, arrange
     const segments = [
         {
             distance: 10,
             date: new Date("2021-03-07T10:00:00")
         }
     ];
-    //when, act
     const fare = calculateRide(segments);
-    //then, assert
     expect(fare).toBe(29);
 
 });
 
 test("Deve fazer uma corrida em um domingo e em um horario noturno", function () {
-    //given, arrange
     const segments = [
         {
             distance: 10,
             date: new Date("2021-03-07T23:00:00")
         }
     ];
-    //when, act
     const fare = calculateRide(segments);
-    //then, assert
     expect(fare).toBe(50);
 
 });
 
 test("Deve retornar -1 se a distancia for invalida", function () {
-    //given, arrange
     const segments = [
         {
             distance: -10,
             date: new Date("2021-03-07T23:00:00")
         }
     ];
-    //when, act
     const fare = calculateRide(segments);
-    //then, assert
     expect(fare).toBe(-1);
 
 });
 test("Deve retornar -2 se a data for invalida", function () {
-    //given, arrange
     const segments = [
         {
             distance: 10,
             date: new Date("invalid date")
         }
     ];
-    //when, act
     const fare = calculateRide(segments);
-    //then, assert
     expect(fare).toBe(-2);
 
 });
