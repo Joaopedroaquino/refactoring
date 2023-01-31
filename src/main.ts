8bbd1f6b77d2262f3afd49a284d2c8cda86b998b
joaopedroaquino <aqerhack@gmail.com>
2023-01-31 20:45:47 -0300

    refactor conditionals

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 // @ts-nocheck
-function isSunday (date){
+function isSunday(date) {
 	return date.getDay() === 0
 }
-function isOvernight(date){
+function isOvernight(date) {
 	return date.getHours() >= 22 || date.getHours() <= 6
 
 }
@@ -11,34 +11,27 @@ export function calculateRide(segments) {
 	for (const segment of segments) {
 		if (segment.distance != null && segment.distance != undefined && typeof segment.distance === "number" && segment.distance > 0) {
 			if (segment.date != null && segment.date != undefined && segment.date instanceof Date && segment.date.toString() !== "Invalid Date") {
-			
-				if (isOvernight(segment.date)) {
-					if (isSunday(segment.date)) {
-						fare += segment.distance * 5;
 
-						// sunday
-					} else {
-						fare += segment.distance * 3.90;
-
-					}
-				} else {
-					// sunday
-					if (isSunday(segment.date)) {
-						fare += segment.distance * 2.9;
-					} else {
-						fare += segment.distance * 2.10;
-					}
-				}
-			} else {
-				return -2;
+				if (isOvernight(segment.date) && isSunday(segment.date)) {
+					fare += segment.distance * 5;}
+				if (isOvernight(segment.date) && !isSunday(segment.date)) {
+					fare += segment.distance * 3.90;}
+			     if (!isOvernight(segment.date) && isSunday(segment.date)) {
+				fare += segment.distance * 2.9;} 
+				 if (!isOvernight(segment.date) && !isSunday(segment.date)) {
+				fare += segment.distance * 2.10;
 			}
-		} else {
-			return -1;
-		}
-	}
-	if (fare < 10) {
-		return 10;
+		
 	} else {
-		return fare;
+		return -2;
 	}
+} else {
+	return -1;
+}
+	}
+if (fare < 10) {
+	return 10;
+} else {
+	return fare;
+}
 }
