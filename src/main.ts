0b1de3facd70c97fa08f0c4f298b7aa52389d22f
joaopedroaquino <aqerhack@gmail.com>
2023-01-31 20:51:05 -0300

    refactor isValidDistance

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,19 @@ function isOvernight(date) {
 	return date.getHours() >= 22 || date.getHours() <= 6
 
 }
+function isValidDistance(distance){
+	return distance != null && distance != undefined && typeof distance === "number" && distance > 0
+}
 export function calculateRide(segments) {
 	let fare = 0;
 	for (const segment of segments) {
-		if (segment.distance != null && segment.distance != undefined && typeof segment.distance === "number" && segment.distance > 0) {
+		if (isValidDistance(segment.distance)) {
 			if (segment.date != null && segment.date != undefined && segment.date instanceof Date && segment.date.toString() !== "Invalid Date") {
-
 				if (isOvernight(segment.date) && isSunday(segment.date)) {
 					fare += segment.distance * 5;}
 				if (isOvernight(segment.date) && !isSunday(segment.date)) {
 					fare += segment.distance * 3.90;}
-			     if (!isOvernight(segment.date) && isSunday(segment.date)) {
+			    if (!isOvernight(segment.date) && isSunday(segment.date)) {
 				fare += segment.distance * 2.9;} 
 				 if (!isOvernight(segment.date) && !isSunday(segment.date)) {
 				fare += segment.distance * 2.10;
