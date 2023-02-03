754eb77b393c46aeadeff8ade0f618120a489ee8
joaopedroaquino <aqerhack@gmail.com>
2023-02-02 21:50:08 -0300

    refactor Ride

--- src/third_version/Ride.ts
+++ src/third_version/Ride.ts
@@ -15,18 +15,18 @@ export default class Ride {
     calculateFare(){
         let fare = 0;
 	for (const segment of this.segments) {
-		if (!isValidDistance(segment.distance)) throw new Error('Invalid distance');
-		if (!isValidDate(segment.date)) throw new Error('Invalid date')
-		if (isOvernight(segment.date) && isSunday(segment.date)) {
+		if (!this.isValidDistance(segment.distance)) throw new Error('Invalid distance');
+		if (!this.isValidDate(segment.date)) throw new Error('Invalid date')
+		if (this.isOvernight(segment.date) && isSunday(segment.date)) {
 			fare += segment.distance * SUNDAY_OVERNIGHT_FARE;
 		}
-		if (isOvernight(segment.date) && !isSunday(segment.date)) {
+		if (this.isOvernight(segment.date) && !isSunday(segment.date)) {
 			fare += segment.distance * OVERNIGHT_FARE;
 		}
-		if (!isOvernight(segment.date) && isSunday(segment.date)) {
+		if (!this.isOvernight(segment.date) && isSunday(segment.date)) {
 			fare += segment.distance * SUNDAY_FARE;
 		}
-		if (!isOvernight(segment.date) && !isSunday(segment.date)) {
+		if (!this.isOvernight(segment.date) && !isSunday(segment.date)) {
 			fare += segment.distance * NORMAL_FARE;
 		}
 	}
