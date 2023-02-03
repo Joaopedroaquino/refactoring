fea1983c940d4bfd8314ad7544223eaac30f186d
joaopedroaquino <aqerhack@gmail.com>
2023-02-02 21:55:32 -0300

    refactor Ride

--- src/third_version/Ride.ts
+++ src/third_version/Ride.ts
@@ -1,34 +1,39 @@
 import Segment from "./Segment";
 
 export default class Ride {
+    SUNDAY_OVERNIGHT_FARE = 5;
+    OVERNIGHT_FARE = 3.90;
+    SUNDAY_FARE = 2.9;
+    NORMAL_FARE = 2.1;
+    MIN_FARE = 10;
     segments: Segment[];
 
-    constructor(){
+    constructor() {
         this.segments = [];
     }
-  
-    addSegment(distance: number, date: Date){
+
+    addSegment(distance: number, date: Date) {
         this.segments.push(new Segment(distance, date))
 
     }
 
-    calculateFare(){
+    calculateFare() {
         let fare = 0;
-	for (const segment of this.segments) {
-		if (segment.isOvernight() && segment.isSunday()) {
-			fare += segment.distance * SUNDAY_OVERNIGHT_FARE;
-		}
-		if (segment.isOvernight() && !segment.isSunday()) {
-			fare += segment.distance * OVERNIGHT_FARE;
-		}
-		if (!segment.isOvernight() && segment.isSunday()) {
-			fare += segment.distance * SUNDAY_FARE;
-		}
-		if (!segment.isOvernight() && !segment.isSunday()) {
-			fare += segment.distance * NORMAL_FARE;
-		}
-	}
-	return (fare < MIN_FARE) ? MIN_FARE : fare;
+        for (const segment of this.segments) {
+            if (segment.isOvernight() && segment.isSunday()) {
+                fare += segment.distance * this.SUNDAY_OVERNIGHT_FARE;
+            }
+            if (segment.isOvernight() && !segment.isSunday()) {
+                fare += segment.distance * this.OVERNIGHT_FARE;
+            }
+            if (!segment.isOvernight() && segment.isSunday()) {
+                fare += segment.distance * this.SUNDAY_FARE;
+            }
+            if (!segment.isOvernight() && !segment.isSunday()) {
+                fare += segment.distance * this.NORMAL_FARE;
+            }
+        }
+        return (fare < this.MIN_FARE) ? this.MIN_FARE : fare;
     }
 
 }
