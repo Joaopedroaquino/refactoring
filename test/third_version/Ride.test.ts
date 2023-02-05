6f0e8dfed3d2ad95ec43e301a1d978321fa7537f
joaopedroaquino <aqerhack@gmail.com>
2023-02-04 21:34:03 -0300

    :white_check_mark: Pass all tests

--- test/third_version/Ride.test.ts
+++ test/third_version/Ride.test.ts
@@ -8,6 +8,28 @@ test("Deve fazer uma corrida em um dia de semana e em horario normal", function 
 
 });
 
+test("Deve fazer uma corrida em um dia de semana e em horario noturno", function () {
+    const ride = new Ride();
+    ride.addSegment( 10, new Date("2021-03-10T23:00:00"));
+   const fare = ride.calculateFare();
+    expect(fare).toBe(39);
+
+});
+test("Deve fazer uma corrida em um domingo e em horario normal", function () {
+    const ride = new Ride();
+    ride.addSegment( 10, new Date("2021-03-07T10:00:00"));
+   const fare = ride.calculateFare();
+    expect(fare).toBe(29);
+
+});
+test("Deve fazer uma corrida em um domingo e em horario noturno", function () {
+    const ride = new Ride();
+    ride.addSegment( 10, new Date("2021-03-07T23:00:00"));
+   const fare = ride.calculateFare();
+    expect(fare).toBe(50);
+
+});
+
 
 
 
