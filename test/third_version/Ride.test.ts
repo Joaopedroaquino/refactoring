ac0eda419acb957d15c8ce3f7d0bd5be19e094b4
joaopedroaquino <aqerhack@gmail.com>
2023-02-08 16:58:28 -0300

    Refactor tests

--- test/third_version/Ride.test.ts
+++ test/third_version/Ride.test.ts
@@ -1,7 +1,12 @@
 import Ride from "../../src/third_version/Ride";
 
+let ride: Ride;
+
+beforeEach(function(){
+    ride = new Ride;
+});
+
 test("Deve fazer uma corrida em um dia de semana e em horario normal", function () {
-    const ride = new Ride();
     ride.addSegment(10, new Date("2021-03-10T10:00:00"));
     const fare = ride.calculateFare();
     expect(fare).toBe(21);
@@ -9,21 +14,18 @@ test("Deve fazer uma corrida em um dia de semana e em horario normal", function 
 });
 
 test("Deve fazer uma corrida em um dia de semana e em horario noturno", function () {
-    const ride = new Ride();
     ride.addSegment(10, new Date("2021-03-10T23:00:00"));
     const fare = ride.calculateFare();
     expect(fare).toBe(39);
 
 });
 test("Deve fazer uma corrida em um domingo e em horario normal", function () {
-    const ride = new Ride();
     ride.addSegment(10, new Date("2021-03-07T10:00:00"));
     const fare = ride.calculateFare();
     expect(fare).toBe(29);
 
 });
 test("Deve fazer uma corrida em um domingo e em horario noturno", function () {
-    const ride = new Ride();
     ride.addSegment(10, new Date("2021-03-07T23:00:00"));
     const fare = ride.calculateFare();
     expect(fare).toBe(50);
@@ -34,19 +36,16 @@ test.each([
     "2021-03-10T07:00:00",
     "2021-03-10T08:30:00",
     "2021-03-10T08:00:00"])("Deve fazer uma corrida em horario de pico %p", function (date) {
-        const ride = new Ride();
-        ride.addSegment(10, new Date(date));
+            ride.addSegment(10, new Date(date));
         const fare = ride.calculateFare();
         expect(fare).toBe(60);
 
     });
 test("Deve lançar um erro se a distancia for invalida", function () {
-    const ride = new Ride();
     expect(() => ride.addSegment(-10, new Date("2021-03-07T23:00:00"))).toThrow(new Error('Invalid distance'))
 
 });
 test("Deve lançar um erro se a data for invalida", function () {
-    const ride = new Ride();
     expect(() => ride.addSegment(10, new Date("any data"))).toThrow(new Error('Invalid date'))
 
 });
