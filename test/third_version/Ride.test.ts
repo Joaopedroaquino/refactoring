28edd051957bb3cf11ca5d1d1a4813ac905287e5
joaopedroaquino <aqerhack@gmail.com>
2023-02-08 14:08:02 -0300

    pass each date test

--- test/third_version/Ride.test.ts
+++ test/third_version/Ride.test.ts
@@ -2,53 +2,57 @@ import Ride from "../../src/third_version/Ride";
 
 test("Deve fazer uma corrida em um dia de semana e em horario normal", function () {
     const ride = new Ride();
-    ride.addSegment( 10, new Date("2021-03-10T10:00:00"));
-   const fare = ride.calculateFare();
+    ride.addSegment(10, new Date("2021-03-10T10:00:00"));
+    const fare = ride.calculateFare();
     expect(fare).toBe(21);
 
 });
 
 test("Deve fazer uma corrida em um dia de semana e em horario noturno", function () {
     const ride = new Ride();
-    ride.addSegment( 10, new Date("2021-03-10T23:00:00"));
-   const fare = ride.calculateFare();
+    ride.addSegment(10, new Date("2021-03-10T23:00:00"));
+    const fare = ride.calculateFare();
     expect(fare).toBe(39);
 
 });
 test("Deve fazer uma corrida em um domingo e em horario normal", function () {
     const ride = new Ride();
-    ride.addSegment( 10, new Date("2021-03-07T10:00:00"));
-   const fare = ride.calculateFare();
+    ride.addSegment(10, new Date("2021-03-07T10:00:00"));
+    const fare = ride.calculateFare();
     expect(fare).toBe(29);
 
 });
 test("Deve fazer uma corrida em um domingo e em horario noturno", function () {
     const ride = new Ride();
-    ride.addSegment( 10, new Date("2021-03-07T23:00:00"));
-   const fare = ride.calculateFare();
+    ride.addSegment(10, new Date("2021-03-07T23:00:00"));
+    const fare = ride.calculateFare();
     expect(fare).toBe(50);
 
 });
-test("Deve fazer uma corrida em horario de pico (7-8 e das 18-19)", function () {
-    const ride = new Ride();
-    ride.addSegment( 10, new Date("2021-03-10T07:30:00"));
-   const fare = ride.calculateFare();
-    expect(fare).toBe(60);
+test.each([
+    "2021-03-10T07:30:00",
+    "2021-03-10T07:00:00",
+    "2021-03-10T08:30:00",
+    "2021-03-10T08:00:00"])("Deve fazer uma corrida em horario de pico (7-8 e das 18-19)", function (date) {
+        const ride = new Ride();
+        ride.addSegment(10, new Date(date));
+        const fare = ride.calculateFare();
+        expect(fare).toBe(60);
 
-});
+    });
 test("Deve lançar um erro se a distancia for invalida", function () {
     const ride = new Ride();
-    expect(()=> ride.addSegment( -10, new Date("2021-03-07T23:00:00"))).toThrow(new Error('Invalid distance'))
+    expect(() => ride.addSegment(-10, new Date("2021-03-07T23:00:00"))).toThrow(new Error('Invalid distance'))
 
 });
 test("Deve lançar um erro se a data for invalida", function () {
     const ride = new Ride();
-    expect(()=> ride.addSegment( 10, new Date("any data"))).toThrow(new Error('Invalid date'))
+    expect(() => ride.addSegment(10, new Date("any data"))).toThrow(new Error('Invalid date'))
 
 });
 test("Deve fazer uma corrida com valor minimo", function () {
-    const ride = new Ride() 
-    ride.addSegment(  1, new Date("2021-03-07T23:00:00"));
+    const ride = new Ride()
+    ride.addSegment(1, new Date("2021-03-07T23:00:00"));
     const fare = ride.calculateFare();
     expect(fare).toBe(10);
 
